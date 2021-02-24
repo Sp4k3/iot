456224a3c9a585469c27350dd14a89c55701b847
Willyan Lin <willyanlin@gmail.com>
2021-02-24 14:05:00 +0100

    feat: 🎸 update speech recognition and add react hook

--- client-react/src/components/Wikipedia.js
+++ client-react/src/components/Wikipedia.js
@@ -89,12 +89,21 @@ class Wikipedia extends React.Component {
                                     Search
                                 </label>
                                 <div className="mt-1">
-                                    <input id="server" name="server" value={this.state.searchValue} onChange={this.handleChange} type="text" required placeholder="terms" className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                    <input
+                                        id="server"
+                                        name="server"
+                                        value={this.state.searchValue}
+                                        onChange={this.handleChange}
+                                        type="text"
+                                        placeholder="terms"
+                                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                 </div>
                             </div>
 
                             <div>
-                                <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                <button
+                                    type="submit"
+                                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                     Search
                                 </button>
                                 <VoiceRecognition callback={this.callbackVoice} />
