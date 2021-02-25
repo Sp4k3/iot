d24d9070736d8f533cc0a22f2fa5c269a8292e9c
Willyan Lin <willyanlin@gmail.com>
2021-02-25 14:33:53 +0100

    feat: 🎸 add Hook to Wiki and Covid

--- client-react/src/components/Wikipedia.js
+++ client-react/src/components/Wikipedia.js
@@ -66,42 +66,40 @@ const Wikipedia = () => {
             <div dangerouslySetInnerHTML={{ __html: searchResult }} />)
         : "";
     return (
-        <div>
-            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                    <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
-                        <div>
-                            <label htmlFor="server" className="block text-sm font-medium text-gray-700">
-                                Search
-                                </label>
-                            <div className="mt-1">
-                                <input
-                                    id="server"
-                                    name="server"
-                                    value={searchValue}
-                                    onChange={handleChange}
-                                    type="text"
-                                    placeholder="terms"
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                            </div>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="server" className="block text-sm font-medium text-gray-700">
+                            Search
+                        </label>
+                        <div className="mt-1">
+                            <input
+                                id="server"
+                                name="server"
+                                value={searchValue}
+                                onChange={handleChange}
+                                type="text"
+                                placeholder="terms"
+                                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                         </div>
+                    </div>
 
-                        <div>
-                            <button
-                                type="submit"
-                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                Search
-                                </button>
-                        </div>
-                    </form>
-                    <VoiceRecognition callback={callbackVoice} />
-                </div>
-                <div className="shortResult">
-                    <cite>{shortResult}</cite>
-                </div>
-                <div className="result">
-                    {result}
-                </div>
+                    <div>
+                        <button
+                            type="submit"
+                            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Search
+                        </button>
+                    </div>
+                </form>
+                <VoiceRecognition callback={callbackVoice} />
+            </div>
+            <div className="shortResult">
+                <cite>{shortResult}</cite>
+            </div>
+            <div className="result">
+                {result}
             </div>
         </div>
     );
